b509d23cb66c8d1c78f0ea305295a029fcd434e8
Logan Wishart-Craig <loganwishartcraig@gmail.com>
2019-05-23 18:14:45 -0700

    Moved BillService

--- src/services/Billing/index.ts
+++ src/services/Billing/index.ts
@@ -1,36 +1,21 @@
-// import { Connection } from 'mongoose';
-// import { DatabaseService, IDatabaseService } from '../Database';
-// import { IBill } from '../../models/Bill';
+import { DatabaseService, IDatabaseService } from '../../Database';
+import { IBill, buildBillModel, INewBillConfig } from '../../../models/Bill';
+import { Connection } from 'mongoose';
 
-// export interface IBillService extends IDatabaseService {
-//     findByEmail(email: string): Promise<IBill | undefined>;
-// }
+export interface IBillService extends IDatabaseService<IBill, INewBillConfig> {
+    getByCreatorId(userId: string): Promise<IBill[]>;
+}
+export class BillService extends DatabaseService<IBill, INewBillConfig> implements IBillService {
 
-// export interface IBillCreate {
-//     readonly email: string;
-// }
+    constructor(connection: Connection) {
+        super({
+            connection,
+            modelFactory: buildBillModel,
+        });
+    }
 
-// export class UserService extends DatabaseService<IBill> implements IBillService {
+    public async getByCreatorId(userId: string): Promise<IBill[]> {
+        return this.find({ createdBy: userId });
+    }
 
-//     constructor(connection: Connection) {
-//         super({
-//             connection,
-//             modelFactory: buildUserModel,
-//         });
-//     }
-
-//     public async findByEmail(email: string): Promise<IUser | undefined> {
-//         return this.findOne({ email });
-//     }
-
-//     protected _formatForInsert(payload: IUserCreate): IUserSchema {
-
-//         return {
-//             email: payload.email,
-//             firstName: payload.firstName,
-//             lastName: payload.lastName,
-//         };
-
-//     }
-
-// }
+}
